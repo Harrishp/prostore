a9b89219476422ac964b3f4c607d886dccbd9e3c
harris <hanpan_harris@163.com>
2025-03-17 20:29:57 +0800

    1.create user button

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,46 +1,67 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCreadentials } from "@/lib/actions/user.actions";
+import { signInDefaultValues } from "@/lib/constants";
+import { useSearchParams } from "next/navigation";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCreadentials, {
+    message: "",
+    success: false,
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button className="w-full" variant="default" disabled={pending}>
+        {pending ? "Signing in..." : "Sign In with credentials"}
+      </Button>
+    );
+  };
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
   return (
-    <form>
+    <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
       <div className="space-y-6">
         <div>
-          <label htmlFor="email">
-            Email
-          </label>
+          <label htmlFor="email">Email</label>
           <Input
             type="email"
             id="email"
             name="email"
             required
-            // defaultValue={signInDefaultValues.email}
+            defaultValue={signInDefaultValues.email}
             autoComplete="email"
           />
         </div>
         <div>
-          <label htmlFor="password">
-            Password
-          </label>
+          <label htmlFor="password">Password</label>
           <Input
             type="password"
             id="password"
             name="password"
             required
-            // defaultValue={signInDefaultValues.password}
+            defaultValue={signInDefaultValues.password}
             autoComplete="current-password"
           />
         </div>
         <div>
-          <Button className="w-full" variant='default'>
-            Sign In with credentials
-          </Button>
+          <SignInButton />
         </div>
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
 
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{" "}
-          <Link target='_self' className="link" href="/sign-up">
+          <Link target="_self" className="link" href="/sign-up">
             Sign Up
           </Link>
         </div>
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,13 +10,25 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import CredentialsSignInForm from "./credentials-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
   description: "Sign in to your account",
 };
 
-const SignIn = () => {
+const SignIn = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+  const session = await auth();
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+
   return (
     <div className="w-full max-w-sm mx-auto">
       <Card>
